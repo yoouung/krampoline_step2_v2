14e2d85e8d697091f10a1561f4117570ab6f3c95
hostdog01 <hostdog01@naver.com>
2023-10-19 07:29:24 +0000

    env test

--- krampoline/src/Main.js
+++ krampoline/src/Main.js
@@ -1,12 +1,16 @@
 import './App.css';
 import logo from './logo.svg'
+
+
 function Main() {
-  
+  const name = process.env.NAME;
+  console.log(process.env);
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
-            모두가 개발자가 된다
+            {name}가 개발자가 된다
         </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
