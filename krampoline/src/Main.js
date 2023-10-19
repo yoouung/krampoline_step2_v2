a1dd2601115f66ea6e1f00bcc75960954d217971
hostdog01 <hostdog01@naver.com>
2023-10-19 02:51:33 +0000

    icon

--- krampoline/src/Main.js
+++ krampoline/src/Main.js
@@ -1,44 +1,14 @@
-import { useState } from 'react';
 import './App.css';
-import { instance } from "./api.js";
-
+import logo from './logo.svg'
 function Main() {
-    
-  const [apiTest, setApiTest] = useState(false);
-  const [dbTest, setDbTest] = useState(false);
-    
-  const handleApiOnclick = async () => {
-      try {
-          await instance.get("/test");
-          setApiTest(true);
-      } catch(err) {
-          console.log(err);
-      }
-      
-  }
-  
-  const handleDbOnClick = async () => {
-      try {
-          await instance.get("/db");
-          setDbTest(true);
-      } catch(err) {
-          console.log(err);
-      }
-  }
   
   return (
     <div className="App">
       <header className="App-header">
-        
         <div>
-          Hello Krampoline !
+            모두가 개발자가 된다
         </div>
-        <div>
-          <button onClick={handleApiOnclick}>API TEST</button><div>{apiTest?"CONNECT":"NOT YET"}</div>
-        </div>
-          <div>
-          <button onClick={handleDbOnClick}>DB TEST</button><div>{dbTest?"CONNECT":"NOT YET"}</div>
-        </div>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
